7881f686293f52c57e9c418a99b32a909ea443bc
David Minaya <dminaya@intellisys.com.do>
2019-10-16 14:51:24 -0400

    Scroll the category section to the top when the ScrollButton is clicked

--- src/Category.js
+++ src/Category.js
@@ -2,14 +2,29 @@ import React from 'react';
 import './Category.css';
 import CategoryStuffedAnimals from './CategoryStuffedAnimals';
 import CategoryWoodenToys from './CategoryWoodenToys';
+import ScrollButton from './ScrollButton';
 
-function Category() {
-    return (
-        <div className="Category">
-            <CategoryStuffedAnimals/>
-            <CategoryWoodenToys/>
-        </div>
-    );
+class Category extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleScrollButtonClick = this.handleScrollButtonClick.bind(this);
+        this.categoryStuffedAnimals = React.createRef();
+    }
+
+    handleScrollButtonClick() {
+        this.categoryStuffedAnimals.current.scrollToTop();
+    }
+
+    render() {
+        return (
+            <div className="Category">
+                <ScrollButton onScrollButtonClick={this.handleScrollButtonClick}/>
+                <CategoryStuffedAnimals ref={this.categoryStuffedAnimals}/>
+                <CategoryWoodenToys/>
+            </div>
+        );
+    }
 }
 
 export default Category;
