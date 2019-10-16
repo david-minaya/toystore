b6af3cac4809151518a10922e2b546f0bc497bde
David Minaya <dminaya@intellisys.com.do>
2019-10-16 15:43:02 -0400

    Join the components of the categories in one component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Hero from './Hero';
-import Category from './Category';
+import Category from './Categories';
 
 function App() {
   return (
